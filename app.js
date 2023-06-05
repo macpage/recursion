dbc7d41f67b746faeb23040df42bcf5e9aa90b34
Mac <sophanpa@gmail.com>
2023-06-05 19:56:23 +0200

    Add while loop to sort the numbers

--- app.js
+++ app.js
@@ -24,3 +24,34 @@ function fibsRec(n) {
 
 console.log(fibsRec(8));
 console.log(arr);
+
+// Mergesort
+let array = [7, 3, 4, 1, 6, 2, 9, 8];
+function merge_sort(arr) {
+  if (arr.length < 2) {
+    return;
+  } else {
+    let left = arr.slice(0, arr.length / 2);
+    let right = arr.slice(arr.length / 2);
+
+    let i = 0;
+    let j = 0;
+    let save = [];
+    console.log(left);
+    console.log(right);
+
+    while (i + j < left.length + right.length) {
+      if (left[i] < right[j]) {
+        save[i + j] = left[i];
+        i++;
+      } else {
+        save[i + j] = right[j];
+        j++;
+      }
+    }
+
+    return (array = save);
+  }
+}
+merge_sort(array);
+console.log(array);
